572933f04580f4cc942d5eb9f96f10d2c4ac20bb
hasanraman <79028159+hasanraman@users.noreply.github.com>
2022-02-17 22:12:39 -0500

    ssecond class second part

--- src/test/java/renastech/day2/GoogleNameLocator.java
+++ src/test/java/renastech/day2/GoogleNameLocator.java
@@ -2,12 +2,13 @@ package renastech.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleNameLocator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //TASK: Google search
         //1- Open a chrome browser
@@ -34,9 +35,43 @@ public class GoogleNameLocator {
         WebElement searchBox=driver.findElement(By.name("q"));
         searchBox.sendKeys("orange");
 
+        Thread.sleep(2000);
         //4- Click google search button
 
         driver.findElement(By.name("btnK")).click();
+
+        //5- Verify title:
+        //Expected: Title should start with “orange” word
+         String expectedTitle="orange";
+         String actualTitle=driver.getTitle();
+         if(actualTitle.startsWith(expectedTitle)){
+             System.out.println("Title verification has passed");
+         }else {
+             System.out.println("Title verification has failed");
+             System.out.println("actualTitle="+actualTitle);
+         }
+
+
+
+        //6-navigate back
+        Thread.sleep(2000);
+        driver.navigate().back();
+
+        //7-write banana in search box
+        Thread.sleep(2000);
+        driver.findElement(By.name("q")).sendKeys("banana"+Keys.ENTER);
+        //8-verify title contains banana.
+        String expectedTitle2="banana";
+        String actualTitle2=driver.getTitle();
+
+        if(actualTitle2.contains(expectedTitle2)){
+            System.out.println("title passed for Banana");
+        }else {
+            System.out.println("Banana verification failed");
+            System.out.println("actual title:" +actualTitle2);
+        }
+
+
 
 
 
